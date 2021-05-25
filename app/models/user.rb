fd06753dc8208bed147f9829429e1a9f0b745b77
Caleb Kivindyo Mutua <mutua.caleb@students.jkuat.ac.ke>
2021-05-25 23:12:11 +0300

    add a static pages controller

--- app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-
-  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  has_many :microposts
-  validates :name, :presence => true, :length => {:maximum => 25}
-  validates :email, :presence => true, :length => {:maximum => 100}, :confirmation => true, :format => EMAIL_REGEX, :uniqueness => true
-
-
-end
