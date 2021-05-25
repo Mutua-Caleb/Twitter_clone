2e3e2239ab46b117e751558984d0394bd45baf8d
Caleb Kivindyo Mutua <mutua.caleb@students.jkuat.ac.ke>
2021-05-25 23:45:09 +0300

    finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,11 +3,24 @@ require "test_helper"
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
-    assert_response :success
+    assert_response :success # this response :success is an abstract representation of the underlying http status code ( in this cases 200 OK)
+    # this test says, let test the home page by issuing a get request to the static pages home URL and then making sure we receive success code in response
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+
   end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+
+    
+  end
+
 end
